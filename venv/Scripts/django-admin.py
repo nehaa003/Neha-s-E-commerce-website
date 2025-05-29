261635cc00d2ca157b15138db20923a57d55130e
Neha <5678nehasingh@gmail.com>
2025-05-29 15:23:11 +0530

    ecommerce website

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Neha\Downloads\ecommerce-master (1)\ecommerce-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
